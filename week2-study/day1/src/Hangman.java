ac086284240de6055b3f8fd976000813e426cc27
OS <oceanshape700@gmail.com>
2021-11-09 23:23:34 +0900

    Update w2d1 - Hangman: Hangman, Main 클래스 분리

---
+++ week2-study/day1/src/Hangman.java
@@ -0,0 +1,13 @@
+public class Hangman {
+
+    private static Hangman singleton = new Hangman();
+
+    public void play() { }
+
+    private Hangman() { }
+
+    public static Hangman getInstance() {
+        return singleton;
+    }
+
+}
