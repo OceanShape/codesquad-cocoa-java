01fd15e8b4246342b37bf9063f54ee3ff0be3771
OS <oceanshape700@gmail.com>
2021-11-23 00:01:20 +0900

    Update w4d1 - GUI: 기본 코드 추가

---
+++ week4-study/day1/GUISample/src/GUITest.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GUITest {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Hello Aptenodytes forsteri");
+        jFrame.setSize(1080, 720);
+        jFrame.setResizable(false);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setLayout(null);
+        jFrame.setVisible(true);
+        jFrame.setLayout(null);
+    }
+}
