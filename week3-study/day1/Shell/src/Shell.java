267772d5dbf7792c9fd91ef0b13f5646814173f8
OS <oceanshape700@gmail.com>
2021-11-22 17:24:22 +0900

    Update w3d1 - Shell: 기본 디렉토리 추가

--- week3-study/day1/Shell/src/Shell.java
+++ week3-study/day1/Shell/src/Shell.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
+enum ShellStatus {
+    RUN,
+    TERMINATE
+}
+
 public class Shell {
+
+    // 어디가 문제인지 확인
+    public String defaultDirectory;
+
+    public String currentDirectory;
+
+    public ShellStatus status = ShellStatus.RUN;
+
+    public void printPrompt() {
+    }
+
+    public Shell() {
+//        this.defaultDirectory = new String(System.getProperty("user.dir" + "\\week3-study\\src"));
+//        this.currentDirectory = new String(defaultDirectory);
+        this.defaultDirectory = System.getProperty("user.dir" + "\\week3-study\\src");
+        this.currentDirectory = defaultDirectory;
+    }
+
+    public void run() {
+        while (status != ShellStatus.TERMINATE) {
+            Scanner sc = new Scanner(System.in);
+            char s = sc.nextLine().charAt(0);
+            if (s == '\n') {
+                status = ShellStatus.TERMINATE;
+            }
+        }
+    }
 }
