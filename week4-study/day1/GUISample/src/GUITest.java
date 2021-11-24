835deff60cb10fbdb78cc9efd0ebe25c2b06449f
OS <oceanshape700@gmail.com>
2021-11-24 11:52:06 +0900

    Update w4d1 - GUI: 닫기 기능 구현

--- week4-study/day1/GUISample/src/GUITest.java
+++ week4-study/day1/GUISample/src/GUITest.java
@@ -1,14 +1,26 @@
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GUITest {
 
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Hello Aptenodytes forsteri");
-        jFrame.setSize(1080, 720);
-        jFrame.setResizable(false);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setLayout(null);
-        jFrame.setVisible(true);
-        jFrame.setLayout(null);
+        Frame f = new Frame();
+        f.setSize(1080, 720);
+        f.setVisible(true);
+        f.addWindowListener(new WindowListener() {
+            public void windowClosing(WindowEvent e){
+                e.getWindow().setVisible(false);
+                e.getWindow().dispose();
+                System.exit(0);
+            }
+
+            public void windowOpened(WindowEvent e) {}
+            public void windowClosed(WindowEvent e){}
+            public void windowIconified(WindowEvent e){}
+            public void windowDeiconified(WindowEvent e){}
+            public void windowActivated(WindowEvent e){}
+            public void windowDeactivated(WindowEvent e){}
+        });
     }
 }
